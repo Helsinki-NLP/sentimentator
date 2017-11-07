50ff7fe750d4b6f6166950019a0f27db19515024
cynarr <32912347+cynarr@users.noreply.github.com>
2017-11-08 00:17:16 +0200

    Refactor db models, add function to query sentences in selected lang

---
+++ models.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+class Sentence(db.Model):
+    """model for one of your table"""
+    __tablename__ = 'sentence'
+    # define your model
+    id = db.Column(db.Integer, primary_key=True)
+    sentence = db.Column(db.String)
+    #source_id = db.Column(db.Integer, db.ForeignKey('source.id'))
+    language_id = db.Column(db.Integer, db.ForeignKey('language.id'))
+    #document_id = db.Column(db.Integer, db.ForeignKey('document.id'))
+
+class Language(db.Model):
+    __tablename__ = 'language'
+    id = db.Column(db.Integer, primary_key=True)
+    language = db.Column(db.String)
+
+class Tag(db.Model):
+    __tablename__ = 'tag'
+    id = db.Column(db.Integer, primary_key=True)
+    sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sentiment_id = db.Column(db.Integer, db.ForeignKey('sentiment.id'))
+
+class Sentiment(db.Model):
+    __tablename__ = 'sentiment'
+    id = db.Column(db.Integer, primary_key=True)
+    sentiment = db.Column(db.String, index=True, unique=True)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+
+class Source(db.Model):
+    __tablename__ = 'source'
+    id = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.String)
+
+class Document(db.Model):
+    __tablename__ = 'document'
+    id = db.Column(db.Integer, primary_key=True)
+    document = db.Column(db.Integer)
--- senti.py
+++ senti.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+from models import db, Language, Sentence
+from sqlalchemy.sql.expression import func
+import random
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///senti.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
 
 @app.route('/')
 def index():
@@ -18,52 +21,17 @@ def language():
 
 @app.route('/annotate')
 def annotate():
-    return render_template('annotate.html')
+    lang = request.args.get('lang')
+    result = Language.query.filter_by(language=lang).first_or_404()
+    #print(type(Language.query.all()))
+    random_sentence = Sentence.query.with_entities(Sentence.sentence)\
+        .filter_by(language_id=result.id).order_by(func.random()).first()
+
+    return render_template('annotate.html', sentence=random_sentence)
 
 @app.route('/logout')
 def logout():
     return render_template('logout.html')
-
-class Sentence(db.Model):
-    """model for one of your table"""
-    __tablename__ = 'sentence'
-    # define your model
-    id = db.Column(db.Integer, primary_key=True)
-    sentence = db.Column(db.String)
-    source_id = db.Column(db.Integer, db.ForeignKey('source.id'))
-    language_id = db.Column(db.Integer, db.ForeignKey('language.id'))
-    document_id = db.Column(db.Integer, db.ForeignKey('document.id'))
-
-class Language(db.Model):
-    __tablename__ = 'language'
-    id = db.Column(db.Integer, primary_key=True)
-    language = db.Column(db.String)
-
-class Tag(db.Model):
-    __tablename__ = 'tag'
-    id = db.Column(db.Integer, primary_key=True)
-    sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    sentiment_id = db.Column(db.Integer, db.ForeignKey('sentiment.id'))
-
-class Sentiment(db.Model):
-    __tablename__ = 'sentiment'
-    id = db.Column(db.Integer, primary_key=True)
-    sentiment = db.Column(db.String, index=True, unique=True)
-
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-
-class Source(db.Model):
-    __tablename__ = 'source'
-    id = db.Column(db.Integer, primary_key=True)
-    source = db.Column(db.String)
-
-class Document(db.Model):
-    __tablename__ = 'document'
-    id = db.Column(db.Integer, primary_key=True)
-    document = db.Column(db.Integer)
 
 if __name__ == '__main__':
     app.run()
