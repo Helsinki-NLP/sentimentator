9e92b730a78f664c11af0aeb6cb9dad707ed82dc
cynarr <32912347+cynarr@users.noreply.github.com>
2017-11-08 00:20:26 +0200

    Cleanup

--- senti.py
+++ senti.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from models import db, Language, Sentence
 from sqlalchemy.sql.expression import func
-import random
 
 app = Flask(__name__)
 
@@ -9,7 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///senti.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
 
 @app.route('/')
 def index():
@@ -23,7 +21,6 @@ def language():
 def annotate():
     lang = request.args.get('lang')
     result = Language.query.filter_by(language=lang).first_or_404()
-    #print(type(Language.query.all()))
     random_sentence = Sentence.query.with_entities(Sentence.sentence)\
         .filter_by(language_id=result.id).order_by(func.random()).first()
 
