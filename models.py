c0cdf97cf072f4f994acf22ba29572a2396688a7
cynarr <32912347+cynarr@users.noreply.github.com>
2017-11-22 19:26:31 +0200

    Add missing values to buttons, fix models

--- models.py
+++ models.py
@@ -26,6 +26,7 @@ class Tag(db.Model):
     sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     sentiment_id = db.Column(db.Integer, db.ForeignKey('sentiment.id'))
+    pnn = db.Column(db.String)
 
 class Sentiment(db.Model):
     __tablename__ = 'sentiment'
@@ -35,6 +36,9 @@ class Sentiment(db.Model):
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    token = db.Column(db.Integer)
 
 class Source(db.Model):
     __tablename__ = 'source'
