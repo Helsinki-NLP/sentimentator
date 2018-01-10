53e6383d87bc22fadb24cb4f949eef8985f7c040
cynarr <32912347+cynarr@users.noreply.github.com>
2018-01-10 18:47:15 +0200

    Fix js to work on more browsers, courtesy of humppa

--- sentimentator/static/js/annotate.js
+++ sentimentator/static/js/annotate.js
@@ -1,46 +1,57 @@
+
 /* annotate.js
  * Hide and reveal user interface elements to create a nice UX flow.
  */
 
-function onCoarseClick() {
+/* Toggle the visibility of the fine sentiment buttons
+ */
+function fineToggle(visibility) {
   var elem = document.querySelectorAll('#fine1, #fine2');
   Array.prototype.forEach.call(elem, function(e) {
-    e.style.display = 'block';
+    e.style.display = visibility;
   });
 }
 
-function onNeutralClick() {
-  document.getElementById('submit').style.display = 'block';
-  var elem = document.querySelectorAll('#fine1, #fine2');
-  Array.prototype.forEach.call(elem, function(e) {
-    e.style.display = 'none';
-  });
-}
-
+/* Reveal submit button
+ */
 function submitReveal() {
   document.getElementById('submit').style.display = 'block';
 }
 
 function init() {
-  var coarse = document.querySelectorAll('#pos, #neg');
+  /* Query for different button elements
+   */
+  var posneg  = document.querySelectorAll('#pos, #neg');
   var neutral = document.querySelectorAll('#neut');
-  var fine   = document.querySelectorAll('#fine1 input, #fine2 input');
-  var hidden = document.querySelectorAll('#fine1, #fine2, #submit');
+  var fine    = document.querySelectorAll('#fine1 input, #fine2 input');
+  var hidden  = document.querySelectorAll('#fine1, #fine2, #submit');
 
-  Array.prototype.forEach.call(coarse, function(e) {
-    e.onchange = onCoarseClick;
+  /* Attach onChange callback functions to each button
+   */
+  Array.prototype.forEach.call(posneg, function(e) {
+    e.onchange = function() {
+      fineToggle('block');
+    }
   });
-    Array.prototype.forEach.call(neutral, function(e) {
-    e.onchange = onNeutralClick;
+  Array.prototype.forEach.call(neutral, function(e) {
+    e.onchange = function() {
+      fineToggle('none');
+      submitReveal();
+    }
   });
   Array.prototype.forEach.call(fine, function(e) {
     e.onchange = submitReveal;
   });
+
+  /* Hide fine sentiments and submit button
+   */
   Array.prototype.forEach.call(hidden, function(e) {
     e.style.display = 'none';
   });
 }
 
+/* Wait for DOM to fully load before starting to tinker with it
+ */
 if (document.attachEvent? document.readyState === "complete": document.readyState !== "loading") {
   init();
 } else {
