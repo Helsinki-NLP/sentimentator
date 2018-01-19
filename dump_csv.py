0ecf9a4a57387b4d0c9fb1509027805fabf065ec
cynarr <32912347+cynarr@users.noreply.github.com>
2018-01-19 15:24:30 +0200

    Edit csv script

--- dump_csv.py
+++ dump_csv.py
@@ -1,13 +1,15 @@
 import sqlite3
 import csv
 
+import pandas
+
 connect_db = sqlite3.connect('sentimentator/db.sqlite')
 output_file = open('en.csv', 'w')
 output_csv = csv.writer(output_file)
 
 cursor = connect_db.execute('select * from sentence')
 
-output_csv.writerow(x[0] for x in cursor.description)
-output_csv.writerows(cursor.fetchall())
+rows = cursor.fetchall()
+output_csv.writerows(rows)
 
 output_file.close()
