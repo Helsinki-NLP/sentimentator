c6940c95d5683109d802eed2e2334307ca611975
cynarr <32912347+cynarr@users.noreply.github.com>
2018-01-24 22:37:12 +0200

    Update dump_csv.py

--- dump_csv.py
+++ dump_csv.py
@@ -7,7 +7,8 @@ connect_db = sqlite3.connect('sentimentator/db.sqlite')
 output_file = open('en.csv', 'w')
 output_csv = csv.writer(output_file)
 
-cursor = connect_db.execute('select sentence.id, sentence.sentence from sentence inner join annotation on sentence.id = annotation.sentence_id where annotation.annotation like "%neg%"')
+# Example: dump all sentences with the annotation label 'neg'
+cursor = connect_db.execute('select sentence.sentence from sentence inner join annotation on sentence.id = annotation.sentence_id where annotation.annotation like "%neg%"')
 
 rows = cursor.fetchall()
 output_csv.writerows(rows)
